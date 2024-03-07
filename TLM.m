e6da7d86c1847a211528bd3403ec19230c1b174a
Michael Passafiume <michaelpassafiume@ad.doe.carleton.ca>
2024-03-07 15:45:03 -0500

    Milestone 7 Code

--- TLM.m
+++ TLM.m
@@ -104,6 +104,14 @@ Zg = sqrt(c_mu_0/c_eps_0)/n_g;
 EtoP = 1/(Zg*f0*v_g*1e-2*c_hb);
 alpha = 0;
 
+%Milestone 7
+beta_r = 0;
+gain_z = gain.*(N-Ntr)./v_g;
+beta_i = (gain_z - alpha)./2;
+beta = beta_r + 1i*beta_i;
+beta_spe = .3e-5;
+gamma = 1.0;
+SPE = 7;
 
 Ef1 = @SourceFct; %Handle creation
 ErN = @SourceFct;
@@ -121,28 +129,49 @@ Ef(1) = InputL(1);          %Replacing location in Ef with location InputL
 Er(Nz) = InputR(1);  
 
 %Milestone 2
-beta = ones(size(z))*(beta_r+1i*beta_i); %Initializing Beta
+%beta = ones(size(z))*(beta_r+1i*beta_i); %Initializing Beta
 exp_det = exp(-1i*dz*beta);
 
-%Milestone 6 Figure
+%Milestone 6-7 Figure
 figure('name', 'Spontainious')
-subplot(3,1,1)
+subplot(3,4,1)
+plot(1);
+xlabel('z(\mum)')
+ylabel('Ef')
+hold off
+subplot(3,4,2)
 plot(z*10000,real(N));
 hold off
 xlabel('z(\mum)')
 ylabel('y')
-subplot(3,1,2)
+subplot(3,4,[3,4])
+plot(time*1e12,real(OutputR),'r');hold on
+plot(time*1e12,real(InputL),'g');
+xlabel('time(ps)')
+ylabel('0utput')
+hold off
+subplot(3,4,[5,6])
 plot(1);
 xlabel('time(ps)')
 ylabel('Nave')
 hold off
-subplot(3,1,3)
+subplot(3,4,[7,8])
+plot(1);
+xlabel('GHz')
+ylabel('20 log|E|')
+hold off
+subplot(3,4,[9,10])
 plot(time*1e12,real(InputL),'r'); hold on
 plot(time*1e12,real(OutputR),'r--'); 
 plot(time*1e12,real(InputR),'b'); hold on
 plot(time*1e12,real(OutputL),'b--');
 xlabel('time(ps)')
 ylabel('E')
+hold off
+subplot(3,4,[11,12])
+plot(1);
+xlabel('GHz')
+ylabel('phase (E)')
 hold off
 
 %Create all initial graphs
@@ -189,6 +218,21 @@ for i = 2:Nt
     Stim = gain.*(N-Ntr).*S;
     N = (N + dt*(I_injv/eVol - Stim))./(1+ dt/taun);
     Nave(i) = mean(N);
+    
+
+    %Milestone7
+    A = sqrt(gamma*beta_spe*c_hb*f0*L*1e-2/taun)/(2*Nz);
+    if SPE > 0
+        Tf = (randn(Nz,1)+1i*randn(Nz,1))*A;
+        Tr = (randn(Nz,1)+1i*randn(Nz,1))*A;
+    else
+        Tf = (ones(Nz,1))*A;
+        Tr = (ones(Nz,1))*A;
+    end
+    EsF = Tf*abs(SPE).*sqrt(N.*1e6);
+    EsR = Tr*abs(SPE).*sqrt(N.*1e6);
+
+
 
     InputL(i) = Ef1(t,InputParasL);
     InputR(i) = ErN(t,0);
@@ -209,6 +253,10 @@ for i = 2:Nt
 
     Ef(2:Nz-1) = Ef(2:Nz-1) - LGain*(Ef(2:Nz-1)-Pf(2:Nz-1));
     Er(2:Nz-1) = Er(2:Nz-1) - LGain*(Er(2:Nz-1)-Pr(2:Nz-1));
+    
+    %Milestone 7
+    Ef = Ef + EsF;
+    Er = Er + EsR;
 
     %Milestone 1
     OutputR(i) = Ef(Nz)*(1-RR);   
@@ -246,21 +294,29 @@ for i = 2:Nt
         % legend('Left Input','Right Output', 'Right Input', 'Left Output', 'Location', 'east')
         % hold off
         
-        subplot(3,1,1)
+        %Milestone 6 plots
+        subplot(3,4,1)
+        plot(real(Ef));hold on
+        plot(imag(Ef));
+        xlabel('z(\mum)')
+        ylabel('Ef')
+        legend('R','i','east')
+        hold off
+        subplot(3,4,2)
         plot(z*10000,real(N));
         xlim(XL*1e4)
         ylim([1e18,5e18])
         xlabel('z(\mum)')
         ylabel('N')
         hold off
-        subplot(3,1,2)
+        subplot(3,4,[5,6])
         plot(time*1e12,real(Nave));
         xlim([0,Nt*dt*1e12])
         ylim()
         xlabel('time(ps)')
         ylabel('Nav')
         hold off
-        subplot(3,1,3);
+        subplot(3,4,[9,10]);
         plot(time*1e12,real(InputL),'r'); hold on
         plot(time*1e12,real(OutputR),'g'); 
         plot(time*1e12,real(InputR),'b');
@@ -278,6 +334,36 @@ for i = 2:Nt
     Pfp = Pf;
     Prp = Pr;
 end
+
+%Milestone 7 plots
+fftEf = fftshift(fft(Ef));
+fftEr = fftshift(fft(Er));
+omega = fftshift(wspace(time));
+
+subplot(3,4,[3,4])
+plot(time*1e12,real(OutputR),'r');hold on
+plot(time*1e12,real(InputL),'g');
+xlim([0,Nt*dt*1e12])
+ylim(YL)
+xlabel('time(ps)')
+ylabel('0utput')
+legend('Output','Input','east')
+hold off
+subplot(3,4,[7,8])
+%plot(omega, unwrap(angle(fftEf))); hold on
+%plot(omega, unwrap(angle(fftEr)));
+% xlim()
+% ylim()
+xlabel('GHz')
+ylabel('20 log|E|')
+hold off
+subplot(3,4,[11,12])
+plot(1);
+% xlim()
+% ylim()
+xlabel('GHz')
+ylabel('phase (E)')
+hold off
 
 % %Milestone 2 Taking FFT of the output and input 
 % fftOutput = fftshift(fft(OutputR));
