c652acc23360ceff7cc624dac39376e8f8b17b15
Michael Passafiume <michaelpassafiume@ad.doe.carleton.ca>
2024-03-08 17:53:20 -0500

    PA 7

--- MN/MN.m
+++ MN/MN.m
@@ -5,7 +5,8 @@ G4 = 1/0.1;
 G0 = 1/1000;
 G5 = 1/1000.1;
 a = 100;
-c = 0.25;
+%c = 0.25;
+c = normrnd(0.25,0.05);
 l = 0.2;
 V1 = 0;
 V2 = 0;
@@ -15,8 +16,13 @@ V5 = 0;
 Iin = 0;
 IL = 0;
 IV = 0;
+w = 1;
 
-Vin = -50;
+%DC
+%Vin = -10;
+%AC
+Vin = 1;
+
 
 %V = [V1 V2 V3 V4 V5 Iin IL IV];
 
@@ -49,14 +55,68 @@ F = [Vin;
     0];
 
 Vfive = [];
+Vone = [];
+Vthree = [];
 imatrix = [];
-%F = G*V + 1i*w*C*V;
+cmatrix = [];
+
+% %DC
+% for i = 1:20
+%     F(1) = F(1) + 1;
+%     Vop = G\F;
+%     Vfive(i) = Vop(5);
+%     imatrix(i) = F(1);
+% end
+% figure 
+% plot(imatrix,Vfive);
+
+% %AC
+% for i = 1:100
+%     w = w + 1;
+%     Vop = (G + 1i*w*C)\F;
+%     Vone(i) = Vop(1);
+%     Vthree(i) = Vop(3);
+%     Vfive(i) = Vop(5);
+%     imatrix(i) = w;
+% end
+% figure
+% subplot(2,1,1)
+% plot(imatrix,real(Vthree)); hold on
+% plot(imatrix,real(Vfive));
+% legend ('three','five','east')
+% hold off
+% gain = real(Vfive)./real(Vone);
+% subplot (2,1,2)
+% histogram(gain,14);
+
+%AC 2
 for i = 1:100
-    F(1) = F(1) + 1;
-    %V = [V1 V2 V3 V4 V5 Iin IL IV];
-    Vop = G\F;
+    c = normrnd(0.25,0.05);
+    C = [0 0 0 0 0 0 0 0;
+    -c c 0 0 0 0 0 0;
+    c -c 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 l 0;
+    0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0;
+    0 0 0 0 0 0 0 0];
+    cmatrix(i) = c;
+    w = 3.14;
+    Vop = (G + 1i*w*C)\F;
+    Vone(i) = Vop(1);
     Vfive(i) = Vop(5);
-    imatrix(i) = F(1);
+    imatrix(i) = i;
 end
 figure
-plot(imatrix,Vfive);
+subplot(2,2,[1,2])
+plot(imatrix,imag(Vfive)); hold on
+plot(imatrix,real(Vfive));
+legend ('imag','real','east')
+hold off
+gain = real(Vfive)./real(Vone);
+subplot(2,2,3)
+histogram(cmatrix,14);
+subplot (2,2,4)
+histogram(gain,14);
+
